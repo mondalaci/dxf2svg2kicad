03a9ec995a436e68653b09ebae7c3fe8ecfe11c6
László Monda <laci@monda.hu>
2013-07-01 13:03:27 +0200

    Call the DXF conversion function for DXF files.  Only use a single FileReader for converting and rendering the input.

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 $(document).ready(function() {
     $('#uploadButton').change(function(event) {
         var file = $('#uploadButton')[0].files[0];
-        var baseFilename = file.name.replace(/\.[^/.]+$/, '');
+        var baseFilename = file.name;
+        var lastDotPosition = baseFilename.lastIndexOf('.');
+        var bareFilename = baseFilename.substr(0, lastDotPosition);
+        var fileExtension = baseFilename.substr(lastDotPosition+1);
 
         $(new FileReader()).load(function(event) {
-            $('#svgImage')[0].src = event.target.result;
-        })[0].readAsDataURL(file);
-
-        $(new FileReader()).load({baseFilename:baseFilename}, function(event) {
-            var svgString = event.target.result;
+            var fileData = event.target.result;
+            var svgString = fileExtension.toLowerCase() == 'dxf' ? dxfToSvg(fileData) : fileData;
+            $('#svgImage')[0].src = 'data:image/svg+xml;utf8,' + svgString;
             var kicadPcb = svgToKicadPcb(svgString, event.data.baseFilename);
             var blob = new Blob([kicadPcb], {type: 'text/plain; charset=utf-8'});
             saveAs(blob, baseFilename+'.kicad_pcb');
         })[0].readAsText(file);
     });
 });
+
+function dxfToSvg(dxfString)
+{
+    var newLines = ['\n\r' /* Windows */, '\n' /* Linux */, '\r' /* Macintosh */];
+}
 
 function svgToKicadPcb(svgString, baseFilename)
 {
