2287748930cea2985531fc03eda5bb663fd4995b
László Monda <laci@monda.hu>
2013-07-02 22:45:41 +0200

    Indent String.format the same way as all the other code.

--- scripts/svgToKicadPcb.js
+++ scripts/svgToKicadPcb.js
@@ -106,15 +106,12 @@ function svgToKicadPcb(svgString, baseFilename)
 
     // Borrowed from http://stackoverflow.com/questions/610406/javascript-equivalent-to-printf-string-format/4673436#4673436
     if (!String.prototype.format) {
-      String.prototype.format = function() {
-        var args = arguments;
-        return this.replace(/{(\d+)}/g, function(match, number) {
-          return typeof args[number] != 'undefined'
-            ? args[number]
-            : match
-          ;
-        });
-      };
+        String.prototype.format = function() {
+            var args = arguments;
+                return this.replace(/{(\d+)}/g, function(match, number) {
+                    return typeof args[number] != 'undefined' ? args[number] : match;
+            });
+        }
     }
 
     function getArcFromPath(path)
