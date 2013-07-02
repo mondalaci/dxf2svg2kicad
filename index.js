40544cb311051b7ce5a7f978c22cb8f51076515e
László Monda <laci@monda.hu>
2013-07-02 14:24:55 +0200

    Add DXF to SVG converter.

--- index.js
+++ index.js
@@ -10,16 +10,81 @@ $(document).ready(function() {
             var fileData = event.target.result;
             var svgString = fileExtension.toLowerCase() == 'dxf' ? dxfToSvg(fileData) : fileData;
             $('#svgImage')[0].src = 'data:image/svg+xml;utf8,' + svgString;
-            var kicadPcb = svgToKicadPcb(svgString, event.data.baseFilename);
+            var kicadPcb = svgToKicadPcb(svgString, baseFilename);
             var blob = new Blob([kicadPcb], {type: 'text/plain; charset=utf-8'});
             saveAs(blob, baseFilename+'.kicad_pcb');
         })[0].readAsText(file);
     });
 });
 
+function deg2rad(deg)
+{
+    return deg * (Math.PI/180);
+}
+
 function dxfToSvg(dxfString)
 {
-    var newLines = ['\n\r' /* Windows */, '\n' /* Linux */, '\r' /* Macintosh */];
+    function dxfObjectToSvgSnippet(dxfObject) {
+        switch (dxfObject.type) {
+            case 'LINE':
+                return _('<path d="M%s,%s L%s,%s"/>').sprintf(dxfObject.x, dxfObject.y, dxfObject.x1, dxfObject.y1);
+            case 'CIRCLE':
+                return _('<circle cx="%s" cy="%s" r="%s"/>').sprintf(dxfObject.x, dxfObject.y, dxfObject.r);
+            case 'ARC':
+                var x1 = dxfObject.x + dxfObject.r * Math.cos(deg2rad(dxfObject.a0));
+                var y1 = dxfObject.y + dxfObject.r * Math.sin(deg2rad(dxfObject.a0));
+                var x2 = dxfObject.x + dxfObject.r * Math.cos(deg2rad(dxfObject.a1));
+                var y2 = dxfObject.y + dxfObject.r * Math.sin(deg2rad(dxfObject.a1));
+                return _('<path d="M%s,%s A%s,%s 0 1,1 %s,%s"/>').sprintf(x1, y1, dxfObject.r, dxfObject.r, x2, y2);
+        }
+    }
+
+    var groupCodes = {
+        0: 'entityType',
+        2: 'blockName',
+        10: 'x',
+        11: 'x1',
+        20: 'y',
+        21: 'y1',
+        40: 'r',
+        50: 'a0',
+        51: 'a1'
+    }
+
+    var counter = 0;
+    var code = null;
+    var entitiesSectionActive = false;
+    var object = {};
+    var svg = '';
+
+    dxfString.split('\r\n').forEach(function(line) {
+        line = line.trim();
+
+        if (counter++ % 2 == 0) {
+            code = parseInt(line);
+        } else {
+            var value = line;
+            if (groupCodes[code] == 'blockName' && value == 'ENTITIES') {
+                entitiesSectionActive = true;
+            } else if (entitiesSectionActive) {
+                if (groupCodes[code] == 'entityType') {
+                    if (object.type) {
+                        svg += dxfObjectToSvgSnippet(object) + '\n';
+                    }
+
+                    object = $.inArray(value, ['LINE', 'CIRCLE', 'ARC']) > -1 ? {type: value} : {};
+
+                    if (value == 'ENDSEC') {
+                        entitiesSectionActive = false;
+                    }
+                } else if (object.type && typeof groupCodes[code] != 'undefined') {
+                    object[groupCodes[code]] = parseFloat(value);
+                }
+            }
+        }
+    });
+
+    return '<svg width="152.9mm" height="125.4mm" viewBox="31.385 -229.628 152.9 125.4" version="1.1" xmlns="http://www.w3.org/2000/svg" style="stroke-linecap:round;stroke-linejoin:round;fill:none"><g transform="scale(1,-1)"   style="stroke:#000000;stroke-width:0.2">' + svg + '</g></svg>';
 }
 
 function svgToKicadPcb(svgString, baseFilename)
